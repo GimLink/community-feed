ae4be3d60b7cc6605c4a72cadef3a46502d65f30
Link <mexlove2@gmail.com>
2025-03-24 15:15:46 +0900

    인수 테스트 환경 구축

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.rest-assured:rest-assured:5.5.0")
+    runtimeOnly("com.h2database:h2")
 
     //mysql
     runtimeOnly("com.mysql:mysql-connector-j")
